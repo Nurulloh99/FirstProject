f27661202e7bbe3c629f55a52e67f84e2e75e487
Nurulloh99 <nooroonus@gmail.com>
2024-11-07 11:52:26 +0500

    numberSummary

--- SRC/Program.cs
+++ SRC/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Son kiriting >> ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var ones = number % 10;
+            var tens = number % 1000 % 100 / 10;
+            var hundreds = number % 1000 / 100;
+            var thousands = number / 1000;
+
+            var summary = ones + tens + hundreds + thousands;
+            Console.WriteLine($"Natija >> {summary}");
+            Console.WriteLine();
+            Main(args);
         }
     }
 }
